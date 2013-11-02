07d41a8672f3cd0f7a5322a5b191824d21e95557
Petar Petrov <p@hintortwo.com>
2013-11-02 22:55:09 +0200

    fix routes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -26,7 +26,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'formcontacts', 'action' => 'add', 'home'));
+	Router::connect('/', array('controller' => 'form_contacts', 'action' => 'add', 'home'));
 	Router::connect('/mrmiyagiprawnsyou', array('controller' => 'formcontacts', 'action' => 'mrmiyagiprawnsyou', 'home'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
